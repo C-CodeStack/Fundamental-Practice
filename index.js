7517808dcc3f378eec004046171b2c1f5585e415
OlGreyFox <87340574+OlGreyFox@users.noreply.github.com>
2024-01-03 14:30:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
-module.exports = problem1 = (h,m,s) => {
+const problem1 = (h,m,s) => {
   
   return 
 }
 
-module.exports = problem2 = (numbers) => {
+const problem2 = (numbers) => {
   
   return 
 }
 
-module.exports = problem3 = (word) => {
+const problem3 = (word) => {
   
   return 
 }
 
-module.exports = problem4 = (num) => {
+const problem4 = (num) => {
   
   return 
 }
 
-module.exports = problem5 = (num1, num2) => {
+const problem5 = (num1, num2) => {
   
   return 
   //in the line above your returning an array. must add .toString() to the end or the test will always fail
 }
 
-module.exports = problem6 = (array) => {
+const problem6 = (array) => {
   
   return 
 }
 
-module.exports = problem7 = (num) => {
+const problem7 = (num) => {
   
   return 
 }
 
-module.exports = problem8 = (array) => {
+const problem8 = (array) => {
   
   return 
   //in the line above your returning an array. must add .toString() to the end or the test will always fail
 }
 
-module.exports = problem9 = (word) => {
+const problem9 = (word) => {
   
   return 
 }
 
-module.exports = problem10 = (color) => {
+const problem10 = (color) => {
   
   return 
 }
+
+module.exports = { problem1, problem2, problem3, problem4, problem5, problem6, problem7, problem8, problem9, problem10 }
